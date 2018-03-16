24ff1ae0e8bb20c4938eb7ad917b1ad8d0097960
Fan Zhang <fanz@linux.com>
2018-03-16 17:03:49 -0400

    nacl c++ wrapper

--- src/sgx/Enclave/enclave_test.cpp
+++ src/sgx/Enclave/enclave_test.cpp
@@ -1,6 +1,3 @@
-//
-// Created by fanz on 8/27/17.
-//
 #include <algorithm>
 
 #include "lest.hpp"
@@ -8,7 +5,7 @@
 #include "bitcoin/uint256.h"
 #include "blockfifo.hpp"
 
-#include "nacl/tweetnacl.h"
+#include "nacl/crypto_box.h"
 
 #include "sgx_trts.h"
 
@@ -30,35 +27,24 @@ const lest::test specification[] =
 int enclaveTest() {
 //  return lest::run(specification);
 
-  unsigned char pk[crypto_box_PUBLICKEYBYTES];
-  unsigned char sk[crypto_box_SECRETKEYBYTES];
-  crypto_box_keypair(pk, sk);
+  string pk, sk;
+  pk = nacl::crypto_box_keypair(&sk);
 
   unsigned char n[crypto_box_NONCEBYTES];
   sgx_read_rand(n, sizeof n);
+  string nonce((char*) n, sizeof n);
 
-  bytes msg {1,2,3,4};
+  string msg {1,2,3,4};
+  string cipher;
 
-  // pad with zeroes
-  msg.insert(msg.begin(), crypto_box_ZEROBYTES, 0);
-
-  bytes cipher(msg.size());
-  std::fill(cipher.begin(), cipher.end(), 0);
-
-  int ret = crypto_box(cipher.data(), msg.data(), msg.size(), n, pk, sk);
-  if (ret) {
-    LL_CRITICAL("crypto box returned %d", ret);
-  }
+  cipher = nacl::crypto_box(msg, nonce, pk, sk);
 
   hexdump("cipher", cipher.data(), cipher.size());
 
-  bytes new_msg(cipher.size());
-  ret = crypto_box_open(new_msg.data(), cipher.data(), cipher.size(), n, pk, sk);
-  if (ret) {
-    LL_CRITICAL("crypto box returned %d", ret);
-  }
+  string plain;
+  plain = nacl::crypto_box_open(cipher, nonce, pk, sk);
 
-  hexdump("msg", new_msg.data(), new_msg.size());
+  hexdump("msg", plain.data(), plain.size());
 
   return 0;
 }
---
+++ src/sgx/Enclave/nacl/crypto_box.h
@@ -0,0 +1,16 @@
+#ifndef NACL_CRYPTO_BOX_H
+#define NACL_CRYPTO_BOX_H
+
+#include <string>
+
+extern "C" {
+#include "tweetnacl.h"
+}
+
+namespace nacl {
+std::string crypto_box_keypair(std::string *sk_string);
+std::string crypto_box(const std::string &m, const std::string &n, const std::string &pk, const std::string &sk);
+std::string crypto_box_open(const std::string &c, const std::string &n, const std::string &pk, const std::string &sk);
+}
+
+#endif //PROJECT_CRYPTO_BOX_H
