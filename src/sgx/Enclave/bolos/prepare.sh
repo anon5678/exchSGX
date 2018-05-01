fe4e06e4d21d6b3a3c74a1bbdc9e7af164854e9b
Fan Zhang <bl4ck5unxx@gmail.com>
2018-05-01 11:57:21 -0400

    add secp256k1 to the working tree

--- src/sgx/Enclave/bolos/prepare.sh
+++ src/sgx/Enclave/bolos/prepare.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-if [[ -d secp256k1 ]]; then rm -rf secp256k1; fi;
+
+set -e
+
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )
+
+pushd ${BASE_DIR}
+
+test -d secp256k1 && rm -rf secp256k1
+
 git clone https://github.com/bitcoin-core/secp256k1
+
 pushd secp256k1
 git checkout 119949232a243396ba1462676932a11022592b59
 patch -p0 < ../patches/secp256k1.patch
-pushd src
 popd
-./autogen.sh && \
-./configure \
-    --disable-shared \
-    --enable-endomorphism \
-    --enable-module-recovery \
-    --enable-tests=no \
-    --enable-openssl-tests=no \
-    --enable-exhaustive-tests=no \
-    --with-bignum=no \
-    --prefix=$PWD/../../secp256k1 && \
-make -j && make install
+
 popd
