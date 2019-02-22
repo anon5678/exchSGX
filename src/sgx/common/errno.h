ff3f837b470e7b299b91042918b319de19d7f3ad
Fan Zhang <bl4ck5unxx@gmail.com>
2019-02-22 13:53:29 -0500

    use constexpr as an optimization

--- src/sgx/common/errno.h
+++ src/sgx/common/errno.h
@@ -1,27 +1,25 @@
 #ifndef PROJECT_ERRNO_H
 #define PROJECT_ERRNO_H
 
-const static int NO_ERROR = 0;
-
 #define SECRETKEY_SEALED_LEN 1024
+
 #define SECKEY_LEN  32
 #define PUBKEY_LEN  65
 #define ADDRESS_LEN 20
-
 #define HYBRID_KEY_NOT_PROVISIONED  -0x9001
+
 #define RSA_KEY_NOT_PROVISIONED     -0x9002
 
+constexpr int NO_ERROR = 0;
+typedef int errno_t;
 
-#define DEFINE_ERROR_CODE(name, reason, mask, num) const static int name##_##reason = -((mask << 16) + num);
-
+#define DEFINE_ERROR_CODE(name, reason, mask, num) constexpr errno_t name##_##reason = -((mask << 16) + num);
 DEFINE_ERROR_CODE(ECALL, UNKNOWN_ERROR, 0x01, 1)
-DEFINE_ERROR_CODE(ECALL, BUFFER_TOO_SMALL, 0x01, 2)
 
+DEFINE_ERROR_CODE(ECALL, BUFFER_TOO_SMALL, 0x01, 2)
 DEFINE_ERROR_CODE(BLOCKFIFO, INVALID_INPUT, 0x10, 1)
 DEFINE_ERROR_CODE(BLOCKFIFO, NOT_A_CHAIN, 0x10, 2)
+
 DEFINE_ERROR_CODE(BLOCKFIFO, INSUFFICIENT_DIFFICULTY, 0x10, 3)
-
-
-typedef int errno_t;
 
 #endif //PROJECT_ERRNO_H
